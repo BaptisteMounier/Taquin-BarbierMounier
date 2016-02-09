cd5e266da700e4994411bcb7731ccee573c40823
BaptisteMounier <baptiste.mounier@hotmail.fr>
2016-02-09 11:46:03 +0100

    Modificaion taille tuiles

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,26 @@ import jeu.*;
 import graphique.*;
 
 public class Main {
+	
+	public final static int WIDTH = Constante.TAILLE_PLATEAU_X
+			* Constante.TAILLE_TUILE_X;
+	
+	public final static int HEIGHT = Constante.TAILLE_PLATEAU_Y
+			* Constante.TAILLE_TUILE_Y;
 
 	public static void main(String[] args) {
 		Jeu jeu = new Jeu();
-		MoteurGraphique moteur = new MoteurGraphique(jeu.getPlateau(), jeu.getTuiles(), Constante.TAILLE_PLATEAU_X, Constante.TAILLE_PLATEAU_Y);
+		MoteurGraphique moteur = new MoteurGraphique(jeu.getPlateau(), jeu.getTuiles(), Main.WIDTH, Main.HEIGHT);
 		
 		JFrame fenetre = new JFrame("Taquin");
 
 		MoteurJeu mj = new MoteurJeu(jeu, moteur);
-
+		fenetre.setPreferredSize(new Dimension(Main.WIDTH,
+				Main.HEIGHT));
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fenetre.setVisible(true);
+		fenetre.setResizable(false);
+		fenetre.pack();
 	}
 
 }
--- src/jeu/Constante.java
+++ src/jeu/Constante.java
@@ -2,8 +2,12 @@ package jeu;
 
 public class Constante {
 	
-	public final static int TAILLE_PLATEAU_X = 3;
+	public static final int TAILLE_PLATEAU_X = 4;
 
-	public final static int TAILLE_PLATEAU_Y = 3;
+	public static final int TAILLE_PLATEAU_Y = 4;
+	
+	public static final int TAILLE_TUILE_X = 100;
+	
+	public static final int TAILLE_TUILE_Y = 100;
 	
 }

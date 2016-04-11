6dad51277282c32a2852af70d4cf448a5063c299
Emsharas <sombreflamme@gmail.com>
2016-04-11 18:04:17 +0200

    modification MainGUI
    ajout du builder du fichier FXML

--- src/application/MainGUI.java
+++ src/application/MainGUI.java
@@ -1,25 +1,44 @@
 package application;
+import java.io.File;
+import java.io.IOException;
+
+import javax.print.DocFlavor.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.*;
 
 public class MainGUI extends Application{
 	
-	public static void main(String[] args) {
-		launch(args);
+	
+	public Scene construitScene() throws IOException{
+		java.net.URL fxmlURL = getClass().getResource("GameView.fxml");
+		final FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+		final Scene node = fxmlLoader.load();
+		return node;
+
 	}
 
-	@Override
 	public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        stage.setTitle("Taquin");
+        stage.setScene(construitScene());
+        
+        
+		Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         boolean add = scene.getStylesheets().add("css/styles.css");
         
         stage.setScene(scene);
         stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 		
 }
